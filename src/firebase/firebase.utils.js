8e18ae102072fac16ec67a8ad1dc719a2e3a2717
Peshotan Irani <59293179+peshotan@users.noreply.github.com>
2022-03-12 14:18:16 -0800

    using github codespace

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,16 +4,15 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBVQcHjmtVtuSn-LMHZtKk5f6aQuOsUpqs",
-    authDomain: "pi2019-react-ecommerce.firebaseapp.com",
-    databaseURL: "https://pi2019-react-ecommerce.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "pi2019-react-ecommerce",
     storageBucket: "pi2019-react-ecommerce.appspot.com",
     messagingSenderId: "927796097845",
-    appId: "1:927796097845:web:2f4ee24616a891f3ac927c",
+    appId: "",
     measurementId: "G-ENWBEVFG1M"
 };
-
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
